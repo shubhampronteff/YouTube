bbf9938b1acb7f2352993137ebc105198eee7811
shubhampronteff <67738104+shubhampronteff@users.noreply.github.com>
2020-09-30 20:20:02 +0530

    30/09/2020

--- App.js
+++ App.js
@@ -14,23 +14,23 @@ const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <Home /> */}
-      {/* <Search /> */}
-      {/* <Content /> */}
-      {/* <Search1 /> */}
-      <Account />
-    </View>
-    // <NavigationContainer>
-    //   <Stack.Navigator headerMode={null}>
-    //     <Stack.Screen name="Home" component={Home} />
-    //     <Stack.Screen name="Search" component={Search} />
-    //     <Stack.Screen name="Content" component={Content} />
-    //     <Stack.Screen name="Options" component={Options} />
-    //     <Stack.Screen name="Hello" component={Hello} />
-    //     <Stack.Screen name="Account" component={Account} />
-    //   </Stack.Navigator>
-    // </NavigationContainer>
+    // <View style={styles.container}>
+    //   {/* <Home /> */}
+    //   {/* <Search /> */}
+    //   {/* <Content /> */}
+    //   {/* <Search1 /> */}
+    //   <Account />
+    // </View>
+    <NavigationContainer>
+      <Stack.Navigator headerMode={null}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Search" component={Search} />
+        <Stack.Screen name="Content" component={Content} />
+        <Stack.Screen name="Options" component={Options} />
+        <Stack.Screen name="Hello" component={Hello} />
+        <Stack.Screen name="Account" component={Account} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
